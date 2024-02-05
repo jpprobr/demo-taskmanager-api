a446a1323caed01384984d8b8b0defe0a0c35416
JP <jpsanet@hotmail.com>
2024-02-05 18:31:56 -0300

    feat: configurações docker, launch e itens relacionados

--- TaskManager.API/Startup.cs
+++ TaskManager.API/Startup.cs
@@ -1,16 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using TaskManager.API.Data.Configurations;
 using TaskManager.API.Data.Repositories;
 
@@ -42,8 +36,9 @@ namespace TaskManager.API
                 builder
                 .AllowAnyMethod()
                 .AllowAnyHeader()
-                .AllowCredentials()
-                .WithOrigins("http://localhost:4200");
+                //.AllowCredentials()
+                //.WithOrigins("http://localhost:4200");
+                .WithOrigins("*");
             }));
         }
 
@@ -52,9 +47,14 @@ namespace TaskManager.API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TaskManager.API v1"));
             }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TaskManager.API v1"));            
 
             app.UseRouting();
 
